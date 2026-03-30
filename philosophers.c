4711942986ec5464e5652d1f213d5a6d484afd3f
olacerda <olacerda@DESKTOP-VUBFROJ.localdomain>
2026-03-30 02:14:30 +0100

    debbug teste

--- philosophers.c
+++ philosophers.c
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   philosophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: otlacerd <otlacerd@student.42.fr>          +#+  +:+       +#+        */
+/*   By: olacerda <olacerda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 21:58:30 by otlacerd          #+#    #+#             */
-/*   Updated: 2026/03/29 07:38:51 by otlacerd         ###   ########.fr       */
+/*   Updated: 2026/03/30 02:14:00 by olacerda         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "philosophers.h"
 #include "parsing.h"
@@ -22,6 +22,7 @@ int	main(int argc, char *argv[])
 	phi = init_structures();
 	if (!core_parsing(argc, argv))
 		return (/*verificar mensagem*/end_structures(phi), 1);
+	dprintf(2, "main branch\n");
 	fill_struct(phi,argv);
 	end_structures(phi);
 	exit(0);
