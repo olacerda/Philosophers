a8c3e7f9a14ea00b0a9a7a9bb6c79925eca6e5c2
olacerda <otaviofficial@gmail.com>
2026-04-03 01:01:29 +0100

    reuploading

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: otlacerd <otlacerd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 21:58:30 by otlacerd          #+#    #+#             */
-/*   Updated: 2026/04/03 00:42:41 by otlacerd         ###   ########.fr       */
+/*   Updated: 2026/04/03 00:59:49 by otlacerd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ int	main(int argc, char *argv[])
 	// dprintf(2, "eat_count main: %li\n", all->param->eat_count);
 	if (!fill_structs(all, argc, argv))
 		return (end_structures("Error fill_structures"), 1);
-	// UL	index = 0;
-	// dprintf(2, "death_time == %lu\n", all->param->death_time);
-	// dprintf(2, "eat_time == %lu\n", all->param->eat_time);
-	// dprintf(2, "eat_count == %li\n", all->param->eat_count);
-	// dprintf(2, "sleep_time == %lu\n", all->param->sleep_time);
-	// dprintf(2, "philo_count == %lu\n", all->param->philo_count);
-	// dprintf(2, "start_time == %lu\n", all->param->start_time);
+	UL	index = 0;
+	dprintf(2, "death_time == %lu\n", all->param->death_time);
+	dprintf(2, "eat_time == %lu\n", all->param->eat_time);
+	dprintf(2, "eat_count == %li\n", all->param->eat_count);
+	dprintf(2, "sleep_time == %lu\n", all->param->sleep_time);
+	dprintf(2, "philo_count == %lu\n", all->param->philo_count);
+	dprintf(2, "start_time == %lu\n", all->param->start_time);
 	while (index < all->param->philo_count)
 	{
 		if (pthread_mutex_init(&all->forks[index], NULL) != 0)
